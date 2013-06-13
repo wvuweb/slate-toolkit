9a0e8e7e9e4fe2778ebd01d1d93e7f3fbb01d183
Adam Johnson <adam.b.johnson1@gmail.com>
2013-06-13 14:37:32 -0400

    Creating an SCSS boilerplate for Slate.

---
+++ default-theme/html5_rhtml/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :compact
+color_output = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To enable debugging comments that display the original location of your selectors, comment:
+line_comments = false
